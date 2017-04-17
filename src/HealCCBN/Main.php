4b96eaa6b10b9d2f6e174b49744a54832d89c6be
pluginernew <nut95@bk.ru>
2017-04-17 04:30:16 +0300

    Update Main.php

--- src/HealCCBN/Main.php
+++ src/HealCCBN/Main.php
@@ -15,4 +15,7 @@ use pocketmine\utils\TextFormat as c;
 class Main extends PluginBase{
 
  public function onEnable(){
-   $this->getLogger()->notice(c: :BOLD.c::DARK_PURPLE."
+   $this->getLogger()->notice(c::BOLD.c::DARK_PURPLE."(I)",c::RESET.c::DARK_AQUA."Has been enabled");
+ }
+ 
+pubilc function onCommand(CommandSender $sender, Command $cmd)
