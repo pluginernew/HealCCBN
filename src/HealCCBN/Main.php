886e4f4629aaee28a3a120508aad6d6b058371d3
pluginernew <nut95@bk.ru>
2017-04-18 04:07:15 +0300

    Update Main.php

--- src/HealCCBN/Main.php
+++ src/HealCCBN/Main.php
@@ -15,7 +15,21 @@ use pocketmine\utils\TextFormat as c;
 class Main extends PluginBase{
 
  public function onEnable(){
-   $this->getLogger()->notice(c::BOLD.c::DARK_PURPLE."(I)",c::RESET.c::DARK_AQUA."Has been enabled");
+   $this->getLogger()->notice(c::BOLD.c::DARK_PURPLE."(|)",c::RESET.c::DARK_AQUA."healplugin Has been enabled");
  }
  
-pubilc function onCommand(CommandSender $sender, Command $cmd)
+pubilc function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+ if(strtolower($cmd->getName()) == "youtube"){
+    if($sender->hasPermission("yt")){
+     $sender->sendMessage(c::BOLD.c::DARK_PURPLE."(|)".c::DARK_AQUA." you have been heald");
+     $sender->setHealth(20.0);
+    }elseif(!$sender->hasPermission("yt")){
+           $sender->sendMessage(c::BOLD.c::DARK_RED."(|)".c: :RESET.c: :RED." Invalad perms");
+    }
+  } 
+}
+ 
+ public function onDisable(){
+  $this->getLogger()->warning(c::BOLD.c::DARK_RED."(|)".c::RESET.c::RED." Healplugin Has been disabled");
+  }
+}
