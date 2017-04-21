20bc57d5138c0cefd25717a2a90912d8067f01ae
pluginernew <nut95@bk.ru>
2017-04-21 06:43:59 +0300

    Update Main.php

--- src/HealCCBN/Main.php
+++ src/HealCCBN/Main.php
@@ -15,21 +15,32 @@ use pocketmine\utils\TextFormat as c;
 class Main extends PluginBase{
 
  public function onEnable(){
-   $this->getLogger()->notice(c::BOLD.c::DARK_PURPLE."(|)",c::RESET.c::DARK_AQUA."healplugin Has been enabled");
+   $this->getLogger()->notice(c::BOLD.c::DARK_PURPLE."(!)",c::RESET.c::DARK_AQUA."healplugin Has been enabled");
  }
  
 pubilc function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
- if(strtolower($cmd->getName()) == "youtube"){
-    if($sender->hasPermission("yt")){
-     $sender->sendMessage(c::BOLD.c::DARK_PURPLE."(|)".c::DARK_AQUA." you have been heald");
-     $sender->setHealth(20.0);
-    }elseif(!$sender->hasPermission("yt")){
-           $sender->sendMessage(c::BOLD.c::DARK_RED."(|)".c: :RESET.c: :RED." Invalad perms");
-    }
-  } 
-}
+ if(!($sender instanceof Player)){
+   $sender->sendMessage(c::BOLD.c::DARK_RED."(!).c::RESET.c::RED." Only Player can run this command");
+   }elseif($sender instanceof Player){
+           if($cmd->getName() == "yt"){
+             if(!(isset($args[0]))){
+               if($sender->hasPermission("yt")){
+                 $sender->sendMessage(c::DARK_PURBLE."/yt".c::DARK_AQUA." heal, feed");
+               }
+              }elseif(!($sender->hasPermission("yt"))){
+                     $sender->sendMessage(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." Invald perms");
+              }
+              if($args[0] == "heal"){
+                if($sender->hasPermssion("yt.heal")){
+                  $sender->sendMessage(c::)
+                
+              
+    
+  
+
+      }
  
  public function onDisable(){
-  $this->getLogger()->warning(c::BOLD.c::DARK_RED."(|)".c::RESET.c::RED." Healplugin Has been disabled");
+  $this->getLogger()->warning(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." Healplugin Has been disabled");
   }
 }
